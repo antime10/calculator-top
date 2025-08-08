d916fa639d59604d29c7c25fc5f59e7868161020
Ryan Deslandes <ryan@que.local>
2025-08-08 12:43:29 +1000

    Add in comments and refactor

--- calculator.js
+++ calculator.js
@@ -1,12 +1,17 @@
-console.log("loading...")
+/* Calculator.js */
 
+// First number loaded into calculator
 let first = null;
+// Second number loaded into calculator
 let second = null;
+// The operator on the inputs
 let operator = null;
+// The result stored between entries
 let result = null;
 
 const output = document.querySelector("#output");
 
+/* Operator functions */
 function add(first, second) {
     return first + second;
 }
@@ -23,12 +28,14 @@ function divide(first, second) {
     return first / second;
 }
 
+/* Run a function on inputs */
 function operate(func, first, second) {
     return func(Number(first), Number(second));
 }
 
 document.querySelectorAll(".number").forEach(
     el => el.addEventListener("click", (el) => {
+	// If it's the decimal point
 	if (
 	    el.target.innerText == "." &&
 		result == null &&
@@ -37,27 +44,39 @@ document.querySelectorAll(".number").forEach(
 	    return;
 	}
 	if (result != null && operator != null) {
+	    // If there's a result and no operator yet store the result
+	    // in the first position and begin inputting to the second
 	    first = result;
 	    second = el.target.innerText;
 	    output.innerText = second;
 	} else if (first == null && operator == null) {
+	    // If there's nothing in the first position or operator begin
+	    // by putting first digit into the first position
 	    first = el.target.innerText;
 	    output.innerText = first;
 	} else if (first != null && operator == null) {
+	    // If we've started inputting into first but still have no
+	    // operator continue inputting into first
 	    first = output.innerText + el.target.innerText;
 	    output.innerText = first;
 	} else if (operator != null && second == null) {
+	    // If the operator isn't null and we don't have anything in
+	    // the second variable start there
 	    second = el.target.innerText;
 	    output.innerText = second;
 	} else if (operator != null && second != null) {
+	    // If we've started inputting into the second variable,
+	    // continue...
 	    second = output.innerText + el.target.innerText;
 	    output.innerText = second;
 	}
-
+	// Always discard result now as it's stored in first or not
+	// needed anymore.
 	result = null;
     })
 )
 
+// Add listeners to operators
 document.querySelectorAll(".operator").forEach(
     el => el.addEventListener("click", (el) => {
 	switch (el.target.innerText) {
@@ -73,15 +92,21 @@ document.querySelectorAll(".operator").forEach(
 	case "/":
 	    operator = divide;
 	    break;
-	}
-	if (el.target.innerText == "=") {
+	case "=":
 	    if (operator == null) {
 		return;
 	    }
+	    break;
+	default:
+	    return;
+	}
+	if (el.target.innerText == "=") {
 	    if (operator == divide && second == "0") {
+		// Divide by zero!
 		output.innerText = "Can't trick me!";
 		return;
 	    }
+	    // Apply the operator to the inputs
 	    result = operate(operator, first, second)
 	    output.innerText = result;
 	    first = null;
